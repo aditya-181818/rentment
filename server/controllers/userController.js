2ddcd46ea95e972db9e1574e407a894575c4e23b
aditya-181818 <adityamaurya05393@gmail.com>
2026-04-10 10:13:33 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,7 @@
 import User from "../models/User.js";
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
-import Product from "../models/product.js";
+import Product from "../models/Product.js";
 
 // Generate JWT Token
 const generateToken = (userId)=>{
